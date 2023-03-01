2657d9a9e7295a9d2a85af0a4574890740ac4f76
ChinhTheHugger <zimmerinker@gmail.com>
2023-03-01 16:37:26 +0700

    commit

--- excel_import_1.py
+++ excel_import_1.py
@@ -3,7 +3,7 @@ from difflib import SequenceMatcher
 from collections import OrderedDict
 #import xlsxwriter
 
-path = "E:\\Pham Thanh Quyet - 23.12.2022\\DSKH 22.12.23\\VRS VRH\\23.02.28 Riverside+ Harmony Full - Tổng hợp khách hàng và căn V17 V4.XLSX"
+path = "E:\\Pham Thanh Quyet - 23.12.2022\\DSKH 22.12.23\\VRS VRH\\23.03.01 Riverside+ Harmony Full - Tổng hợp khách hàng và căn V17 V4 - Copy - Copy.XLSX"
 
 wb_obj = openpyxl.load_workbook(path)
 sheet_obj = wb_obj.active
@@ -113,7 +113,7 @@ sheet_obj = wb_obj.active
 #     sheet_obj.cell(row = x, column = 12).value = sheet_obj.cell(row = x, column = 11).value
 #     for i in range(x+1,x+5000):
 #         if sheet_obj.cell(row = i, column = 4).value == sheet_obj.cell(row = x, column = 4).value:
-#             sheet_obj.cell(row = x, column = 12).value = sheet_obj.cell(row = x, column = 12).value + "&"+ sheet_obj.cell(row = i, column = 11).value
+#             sheet_obj.cell(row = x, column = 12).value = sheet_obj.cell(row = x, column = 12).value + "&" + sheet_obj.cell(row = i, column = 11).value
 #             sheet_obj.cell(row = i, column = 3).value = None
 #             continue
 #         else:
@@ -135,11 +135,11 @@ sheet_obj = wb_obj.active
 
 # * Check houses of each person in a shared phone group and remove unique houses of people with large number of houses to reduce string length
 
-for x in range(2,sheet_obj.max_row+1):
-    waymark = 0
-    if sheet_obj.cell(row = x, column = 2).value == "BIỆN XUÂN KHEN":
-        waymark = x
-    for i in range(waymark+1,2000):
-        if sheet_obj.cell(row = i, column = 2).value == "BIỆN XUÂN KHEN":
-            sheet_obj.cell(row = i, column = 14).value = "x"    
+# for x in range(2,sheet_obj.max_row+1):
+#     waymark = 0
+#     if sheet_obj.cell(row = x, column = 2).value == "BIỆN XUÂN KHEN":
+#         waymark = x
+#     for i in range(waymark+1,2000):
+#         if sheet_obj.cell(row = i, column = 2).value == "BIỆN XUÂN KHEN":
+#             sheet_obj.cell(row = i, column = 14).value = "x"    
 wb_obj.save(path)
--- excel_import_2.py
+++ excel_import_2.py
@@ -1,7 +1,7 @@
 import openpyxl
 from collections import Counter
 
-path = "E:\\Pham Thanh Quyet - 23.12.2022\\DSKH 22.12.23\\VRS VRH\\23.02.28 Riverside+ Harmony Full - Tổng hợp khách hàng và căn V17 V4.XLSX"
+path = "E:\\Pham Thanh Quyet - 23.12.2022\\DSKH 22.12.23\\VRS VRH\\23.03.01 Riverside+ Harmony Full - Tổng hợp khách hàng và căn V18.XLSX"
 
 wb_obj = openpyxl.load_workbook(path)
 sheet_obj = wb_obj.active
@@ -10,57 +10,59 @@ sheet_obj = wb_obj.active
 
 # * Count general phone and house
 
-# for x in range(2,sheet_obj.max_row+1):
-#     #phone
-#     phoneCount = sheet_obj.cell(row = x, column = 16).value
-#     sheet_obj.cell(row = x, column = 14).value = phoneCount.count(';') + 1
-#     #house
-#     houseCount = sheet_obj.cell(row = x, column = 3).value
-#     sheet_obj.cell(row = x, column = 13).value = houseCount.count(';') + 1
+for x in range(2,sheet_obj.max_row+1):
+    #phone
+    phoneCount = ""
+    phoneCount = sheet_obj.cell(row = x, column = 16).value
+    sheet_obj.cell(row = x, column = 14).value = phoneCount.count(';') + 1
+    #house
+    houseCount = ""
+    houseCount = sheet_obj.cell(row = x, column = 3).value
+    sheet_obj.cell(row = x, column = 13).value = houseCount.count(';') + 1
 
 # ***
 
 # * Count house in each area
 
-# for x in range(2,sheet_obj.max_row+1):
-#     #VRSH
-#     areaCount = sheet_obj.cell(row = x, column = 3).value
-#     sheet_obj.cell(row = x, column = 5).value = areaCount.count('VRSH')
-#     #VOP1
-#     areaCount = sheet_obj.cell(row = x, column = 3).value
-#     sheet_obj.cell(row = x, column = 6).value = areaCount.count('VOP1')
-#     #VOP2
-#     areaCount = sheet_obj.cell(row = x, column = 3).value
-#     sheet_obj.cell(row = x, column = 7).value = areaCount.count('VOP2')
-#     #ECP
-#     areaCount = sheet_obj.cell(row = x, column = 3).value
-#     sheet_obj.cell(row = x, column = 8).value = areaCount.count('ECP')
-#     #STL
-#     areaCount = sheet_obj.cell(row = x, column = 3).value
-#     sheet_obj.cell(row = x, column = 9).value = areaCount.count('STL')
-#     #VGP
-#     areaCount = sheet_obj.cell(row = x, column = 3).value
-#     sheet_obj.cell(row = x, column = 10).value = areaCount.count('VGP')
-#     #VGV
-#     areaCount = sheet_obj.cell(row = x, column = 3).value
-#     sheet_obj.cell(row = x, column = 11).value = areaCount.count('VGV')
+for x in range(2,sheet_obj.max_row+1):
+    #VRSH
+    areaCount = sheet_obj.cell(row = x, column = 3).value
+    sheet_obj.cell(row = x, column = 5).value = areaCount.count('VRSH')
+    #VOP1
+    areaCount = sheet_obj.cell(row = x, column = 3).value
+    sheet_obj.cell(row = x, column = 6).value = areaCount.count('VOP1')
+    #VOP2
+    areaCount = sheet_obj.cell(row = x, column = 3).value
+    sheet_obj.cell(row = x, column = 7).value = areaCount.count('VOP2')
+    #ECP
+    areaCount = sheet_obj.cell(row = x, column = 3).value
+    sheet_obj.cell(row = x, column = 8).value = areaCount.count('ECP')
+    #STL
+    areaCount = sheet_obj.cell(row = x, column = 3).value
+    sheet_obj.cell(row = x, column = 9).value = areaCount.count('STL')
+    #VGP
+    areaCount = sheet_obj.cell(row = x, column = 3).value
+    sheet_obj.cell(row = x, column = 10).value = areaCount.count('VGP')
+    #VGV
+    areaCount = sheet_obj.cell(row = x, column = 3).value
+    sheet_obj.cell(row = x, column = 11).value = areaCount.count('VGV')
 
 # ***
 
 # * Count total area
 
-# for x in range(2,sheet_obj.max_row+1):
-#     count = 0
-#     for i in range(5,11):
-#         if sheet_obj.cell(row = x, column = i).value > 0:
-#             count += 1
-#     sheet_obj.cell(row = x, column = 12).value = count
+for x in range(2,sheet_obj.max_row+1):
+    count = 0
+    for i in range(5,11):
+        if sheet_obj.cell(row = x, column = i).value > 0:
+            count += 1
+    sheet_obj.cell(row = x, column = 12).value = count
 
 # ***
 
 # * Count total house
 
 for x in range(2,sheet_obj.max_row+1):
-    areaCount = sheet_obj.cell(row = x, column = 10).value
+    areaCount = sheet_obj.cell(row = x, column = 3  ).value
     sheet_obj.cell(row = x, column = 13).value = areaCount.count(';')+1
 wb_obj.save(path)
