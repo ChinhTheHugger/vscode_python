622c541fca504e3d00f0dfde89ef7206449c9a2c
ChinhTheHugger <zimmerinker@gmail.com>
2023-04-05 17:20:53 +0700

    Commit

--- new_logic copy.py
+++ new_logic copy.py
@@ -4,7 +4,7 @@ import time
 import numpy
 
 pathTest = "E:\\Pham Thanh Quyet - 23.12.2022\\DSKH 22.12.23\\VRS VRH\\Book.XLSX"
-path = "E:\\Pham Thanh Quyet - 23.12.2022\\DSKH 22.12.23\\VRS VRH\\23.03.29 Riverside+ Harmony Full - Tổng hợp khách hàng và căn V23 - for processing.XLSX"
+path = "E:\\Pham Thanh Quyet - 23.12.2022\\DSKH 22.12.23\\VRS VRH\\23.04.05 Riverside+ Harmony Full - Tổng hợp khách hàng và căn V23 - for processing.XLSX"
 
 wb_obj = openpyxl.load_workbook(path)
 sheet_obj = wb_obj.active
@@ -270,43 +270,43 @@ start = time.time()
 # wb_test=openpyxl.load_workbook(pathTest)
 # sh_test=wb_test.active
 
-def cleanHouse(arr):
-    tmpStr=';'.join(arr)
-    tmpArr=tmpStr.split(';')
-    tmpArr=list(dict.fromkeys(tmpArr))
-    tmpStr=';'.join(tmpArr)
-    return tmpStr
+# def cleanHouse(arr):
+#     tmpStr=';'.join(arr)
+#     tmpArr=tmpStr.split(';')
+#     tmpArr=list(dict.fromkeys(tmpArr))
+#     tmpStr=';'.join(tmpArr)
+#     return tmpStr
 
-def houseGroup(x,i):
-    if i-x==0:
-        sheet_obj.cell(row=x,column=15).value=sheet_obj.cell(row=x,column=12).value
-    else:
-        for a in range(x,i+1):
-            sheet_obj.cell(row=a,column=15).value=sheet_obj.cell(row=a,column=12).value
-            houseArr=[]
-            houseArr.append(str(sheet_obj.cell(row=a,column=15).value))
-            for b in range(x,i+1):
-                if str(sheet_obj.cell(row=a,column=3).value) in str(sheet_obj.cell(row=b,column=14).value):
-                    houseArr.append(str(sheet_obj.cell(row=b,column=12).value))
-            houseStr=cleanHouse(houseArr)
-            sheet_obj.cell(row=a,column=15).value=houseStr
-    return
+# def houseGroup(x,i):
+#     if i-x==0:
+#         sheet_obj.cell(row=x,column=15).value=sheet_obj.cell(row=x,column=12).value
+#     else:
+#         for a in range(x,i+1):
+#             sheet_obj.cell(row=a,column=15).value=sheet_obj.cell(row=a,column=12).value
+#             houseArr=[]
+#             houseArr.append(str(sheet_obj.cell(row=a,column=15).value))
+#             for b in range(x,i+1):
+#                 if str(sheet_obj.cell(row=a,column=3).value) in str(sheet_obj.cell(row=b,column=14).value):
+#                     houseArr.append(str(sheet_obj.cell(row=b,column=12).value))
+#             houseStr=cleanHouse(houseArr)
+#             sheet_obj.cell(row=a,column=15).value=houseStr
+#     return
 
-arr = [2]
-for x in range(2,sheet_obj.max_row+1):
-    for i in range(x+1,sheet_obj.max_row+2):
-        if sheet_obj.cell(row=i,column=2).value != sheet_obj.cell(row=x,column=2).value:
-            break
-    if i not in arr:
-        arr.append(i)
-    x=i
+# arr = [2]
+# for x in range(2,sheet_obj.max_row+1):
+#     for i in range(x+1,sheet_obj.max_row+2):
+#         if sheet_obj.cell(row=i,column=2).value != sheet_obj.cell(row=x,column=2).value:
+#             break
+#     if i not in arr:
+#         arr.append(i)
+#     x=i
 
-print(arr)
-print(len(arr))
+# print(arr)
+# print(len(arr))
 
-newArr = numpy.array(arr)
-for x in range(0,len(arr)-1):
-    houseGroup(newArr[x],newArr[x+1]-1)
+# newArr = numpy.array(arr)
+# for x in range(0,len(arr)-1):
+#     houseGroup(newArr[x],newArr[x+1]-1)
 
 # Phone group
 
@@ -348,7 +348,7 @@ for x in range(0,len(arr)-1):
 # for x in range(0,len(arr)-1):
 #     phoneGroup(newArr[x],newArr[x+1]-1)
 
-# ***
+# !!!!! *** !!!!!
 
 # * Fifth step, different from previous four steps
 
@@ -361,6 +361,166 @@ for x in range(0,len(arr)-1):
 # But even after 4 steps, still cant be grouped together
 # Which mean even the original data of the combos already have no similarity from the start
 # So let's try grouping using the result from the same step
+
+# House group
+
+# def cleanHouse(arr):
+#     tmpStr=';'.join(arr)
+#     tmpArr=tmpStr.split(';')
+#     tmpArr=list(dict.fromkeys(tmpArr))
+#     tmpStr=';'.join(tmpArr)
+#     return tmpStr
+
+# def houseGroup(x,i):
+#     if i-x==0:
+#         sheet_obj.cell(row=x,column=7).value=sheet_obj.cell(row=x,column=5).value
+#     else:
+#         for a in range(x,i+1):
+#             sheet_obj.cell(row=a,column=7).value=sheet_obj.cell(row=a,column=5).value
+#             houseArr=[]
+#             houseArr.append(str(sheet_obj.cell(row=a,column=7).value))
+#             for b in range(x,i+1):
+#                 if str(sheet_obj.cell(row=a,column=3).value) in str(sheet_obj.cell(row=b,column=6).value):
+#                     houseArr.append(str(sheet_obj.cell(row=b,column=5).value))
+#             houseStr=cleanHouse(houseArr)
+#             sheet_obj.cell(row=a,column=7).value=houseStr
+#     return
+
+# arr = [2]
+# for x in range(2,sheet_obj.max_row+1):
+#     for i in range(x+1,sheet_obj.max_row+2):
+#         if sheet_obj.cell(row=i,column=2).value != sheet_obj.cell(row=x,column=2).value:
+#             break
+#     if i not in arr:
+#         arr.append(i)
+#     x=i
+
+# print(arr)
+# print(len(arr))
+
+# newArr = numpy.array(arr)
+# for x in range(0,len(arr)-1):
+#     houseGroup(newArr[x],newArr[x+1]-1)
+
+# Phone group
+
+# def cleanPhone(arr):
+#     tmpStr=';'.join(arr)
+#     tmpArr=tmpStr.split(';')
+#     tmpArr=list(dict.fromkeys(tmpArr))
+#     tmpStr=';'.join(tmpArr)
+#     return tmpStr
+
+# def phoneGroup(x,i):
+#     if i-x==0:
+#         sheet_obj.cell(row=x,column=8).value=sheet_obj.cell(row=x,column=6).value
+#     else:
+#         for a in range(x,i+1):
+#             sheet_obj.cell(row=a,column=8).value=sheet_obj.cell(row=a,column=6).value
+#             phoneArr=[]
+#             phoneArr.append(str(sheet_obj.cell(row=a,column=8).value))
+#             for b in range(x,i+1):
+#                 if str(sheet_obj.cell(row=a,column=4).value) in str(sheet_obj.cell(row=b,column=7).value):
+#                     phoneArr.append(str(sheet_obj.cell(row=b,column=6).value))
+#             phoneStr=cleanPhone(phoneArr)
+#             sheet_obj.cell(row=a,column=8).value=phoneStr
+#     return
+
+# arr = [2]
+# for x in range(2,sheet_obj.max_row+1):
+#     for i in range(x+1,sheet_obj.max_row+2):
+#         if sheet_obj.cell(row=i,column=2).value != sheet_obj.cell(row=x,column=2).value:
+#             break
+#     if i not in arr:
+#         arr.append(i)
+#     x=i
+
+# print(arr)
+# print(len(arr))
+
+# newArr = numpy.array(arr)
+# for x in range(0,len(arr)-1):
+#     phoneGroup(newArr[x],newArr[x+1]-1)
+
+# House group
+
+# def cleanHouse(arr):
+#     tmpStr=';'.join(arr)
+#     tmpArr=tmpStr.split(';')
+#     tmpArr=list(dict.fromkeys(tmpArr))
+#     tmpStr=';'.join(tmpArr)
+#     return tmpStr
+
+# def houseGroup(x,i):
+#     if i-x==0:
+#         sheet_obj.cell(row=x,column=9).value=sheet_obj.cell(row=x,column=7).value
+#     else:
+#         for a in range(x,i+1):
+#             sheet_obj.cell(row=a,column=9).value=sheet_obj.cell(row=a,column=7).value
+#             houseArr=[]
+#             houseArr.append(str(sheet_obj.cell(row=a,column=9).value))
+#             for b in range(x,i+1):
+#                 if str(sheet_obj.cell(row=a,column=3).value) in str(sheet_obj.cell(row=b,column=8).value):
+#                     houseArr.append(str(sheet_obj.cell(row=b,column=7).value))
+#             houseStr=cleanHouse(houseArr)
+#             sheet_obj.cell(row=a,column=9).value=houseStr
+#     return
+
+# arr = [2]
+# for x in range(2,sheet_obj.max_row+1):
+#     for i in range(x+1,sheet_obj.max_row+2):
+#         if sheet_obj.cell(row=i,column=2).value != sheet_obj.cell(row=x,column=2).value:
+#             break
+#     if i not in arr:
+#         arr.append(i)
+#     x=i
+
+# print(arr)
+# print(len(arr))
+
+# newArr = numpy.array(arr)
+# for x in range(0,len(arr)-1):
+#     houseGroup(newArr[x],newArr[x+1]-1)
+
+# Phone group
+
+# def cleanPhone(arr):
+#     tmpStr=';'.join(arr)
+#     tmpArr=tmpStr.split(';')
+#     tmpArr=list(dict.fromkeys(tmpArr))
+#     tmpStr=';'.join(tmpArr)
+#     return tmpStr
+
+# def phoneGroup(x,i):
+#     if i-x==0:
+#         sheet_obj.cell(row=x,column=10).value=sheet_obj.cell(row=x,column=8).value
+#     else:
+#         for a in range(x,i+1):
+#             sheet_obj.cell(row=a,column=10).value=sheet_obj.cell(row=a,column=8).value
+#             phoneArr=[]
+#             phoneArr.append(str(sheet_obj.cell(row=a,column=10).value))
+#             for b in range(x,i+1):
+#                 if str(sheet_obj.cell(row=a,column=4).value) in str(sheet_obj.cell(row=b,column=9).value):
+#                     phoneArr.append(str(sheet_obj.cell(row=b,column=8).value))
+#             phoneStr=cleanPhone(phoneArr)
+#             sheet_obj.cell(row=a,column=10).value=phoneStr
+#     return
+
+# arr = [2]
+# for x in range(2,sheet_obj.max_row+1):
+#     for i in range(x+1,sheet_obj.max_row+2):
+#         if sheet_obj.cell(row=i,column=2).value != sheet_obj.cell(row=x,column=2).value:
+#             break
+#     if i not in arr:
+#         arr.append(i)
+#     x=i
+
+# print(arr)
+# print(len(arr))
+
+# newArr = numpy.array(arr)
+# for x in range(0,len(arr)-1):
+#     phoneGroup(newArr[x],newArr[x+1]-1)
 
 
 
